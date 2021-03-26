5936560f7912c7f46d7280a9e110419dd7e343dc
manu <manujinete@gmail.com>
2021-03-26 20:56:58 +0100

    Organizo Urls

---
+++ ProyectoWebApp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from ProyectoWebApp import views
+
+urlpatterns = [
+    
+    path('', views.home, name="Home"),
+    path('servicios', views.servicios, name="Servicios"),
+    path('tienda', views.tienda, name="Tienda"),
+    path('blog', views.blog, name="Blog"),
+    path('contactos', views.contactos, name="Contactos"),
+
+
+]
