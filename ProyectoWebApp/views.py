e1919b40b9e378601d2ee4cac9c4bfc9b7eeda6f
manu <manujinete@gmail.com>
2021-03-26 20:35:12 +0100

    creamos las 5 views y urls

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+
+    return HttpResponse("Home") #texto que nos responde.
+
+def servicios(request):
+    
+    return HttpResponse("Servicios")
+
+def tienda(request):
+    
+    return HttpResponse("Tienda")
+
+def blog(request):
+    
+    return HttpResponse("Blog")
+
+def contactos(request):
+    
+    return HttpResponse("Contactos")
